b8c89f4c39b51c7ada6f1aa9a8f49beaea95c050
Ananya Nadig <ananyarnadig@gmail.com>
2021-09-29 01:31:54 -0400

    file_exists

--- test/download_data.py
+++ test/download_data.py
@@ -3,7 +3,7 @@ from os import path
 
 def main():
 
-   print ("File exists:"+str(path.exists('download_script.py')))
+   print ("File exists:"+str(path.exists('src/download_script.py')))
 
 if __name__== "__main__":
    main()
