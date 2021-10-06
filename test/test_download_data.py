aeda6272af6cdc6f8cad851448fffd8a1a0e1d4d
Ananya Nadig <ananyarnadig@gmail.com>
2021-10-06 00:09:09 -0400

    fixedtests

--- test/test_download_data.py
+++ test/test_download_data.py
@@ -1,23 +1,9 @@
-#import os.path
-#import unittest
-#from os import path
-
-#def main(self):
-
-   #print ("File exists:"+str(path.exists('src/download_script.py')))
- #  self.assertEquals("SpencerIsAwesome", "SpencerIsAwesome")
-
-#if __name__== "__main__":
- #  main()
-
-
 import os.path
 import unittest
 from os import path
 
 class VisualTest(unittest.TestCase):
     def test_data_download(self):
-        #self.assertEqual("SpencerIsAwesome", "SpencerIsAwesome")
         path.exists('src/download_script.py')
 
 
