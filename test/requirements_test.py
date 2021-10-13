17a05cfe6edf126b4201e953ac47cf89d0d4ff08
Siddhesh-Gupta <66965819+Siddhesh-Gupta@users.noreply.github.com>
2021-10-12 20:22:44 -0700

    Update requirements_test.py

--- test/requirements_test.py
+++ test/requirements_test.py
@@ -1,9 +1,14 @@
 
-import sys
+from pip._internal.utils.misc import get_installed_distributions 
 
 def check():
+    
+    lst = get_installed_distributions()
+    flat_installed_packages = [package.project_name for package in lst]
+        
     checked = False
-    if "jupyterlab" in sys.modules:
+    
+    if "jupyterlab" in flat_installed_packages:
         checked = True
         
     assert(checked == True)
