581c05046e72090ffff8cd0279f65f49372c7968
Siddhesh-Gupta <66965819+Siddhesh-Gupta@users.noreply.github.com>
2021-10-12 19:34:22 -0700

    Update requirements_test.py

--- test/requirements_test.py
+++ test/requirements_test.py
@@ -4,7 +4,7 @@ from os import path
 
 class VisualTest(unittest.TestCase):
    def test_data_download(self):
-      path.exists('src/requirements-test.py')
+      path.exists('src/requirements.txt')
 
 if __name__== "__main__":
    unittest.main()
