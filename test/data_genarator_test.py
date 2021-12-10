bcc6cb28628c4f275d389c31131d9b733d3783d8
SpencerPotter <89940944+SpencerPotter@users.noreply.github.com>
2021-12-10 17:34:53 -0500

    comment

--- test/data_genarator_test.py
+++ test/data_genarator_test.py
@@ -8,15 +8,14 @@ class MyTestCase(unittest.TestCase):
     def my_data_generator(self):
         filePath = "data/cv-valid-train.csv"
         dataProcessor = dataProcess(filePath, ['text', 'up_votes', 'down_votes', 'age', 'accent', 'duration'], 5000, 20)
-        dataProcessing = dataProcessor.my_dataProcess()
-        dataProcessing.createDataframeAndDrop()
-        dataProcessing.sliceAndTrim()
-        dataGen = DataGenerator(batch_size=8, dataset=dataProcessing.dataHolder, n_mfccs=40)
+        dataProcessor.createDataframeAndDrop()
+        dataProcessor.sliceAndTrim()
+        dataGen = DataGenerator(batch_size=8, dataset=dataProcessor.dataHolder, n_mfccs=40)
         return dataGen
 
-    def test_batch_size(dataGen):
+    """def test_batch_size(dataGen):
         data_gen = dataGen.my_data_generator()
-        assert len(data_gen.__getitem__(1)[0]) == data_gen.batch_size
+        assert len(data_gen.__getitem__(1)[0]) == data_gen.batch_size"""
 
     """def test_generate_x(dataGen):
         data_gen = dataGen.my_data_generator()
